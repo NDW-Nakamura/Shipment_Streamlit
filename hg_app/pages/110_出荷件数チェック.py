b84496c440a95d296f395322eec9d533c25e50f4
NDW-Nakamura <115513002+NDW-Nakamura@users.noreply.github.com>
2022-10-11 17:36:03 +0900

    Update 110_出荷件数チェック.py

--- hg_app/pages/110_出荷件数チェック.py
+++ hg_app/pages/110_出荷件数チェック.py
@@ -86,7 +86,7 @@ if increment:
     st.session_state.count += 1 # (A)
 
 if st.session_state.count % 2 == 0:
-    image = Image.open('hg_app\\1.png')
+    image = Image.open('\\hg_app\\1.png')
     st.image(image)
 
     image2 = Image.open('image\\3.png')
