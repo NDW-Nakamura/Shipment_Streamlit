a4fe6d37d01168417a0c517a42b2bcccd6f19f8a
NEXUS DIGITAL WORKS <115513002+NDW-Nakamura@users.noreply.github.com>
2023-02-10 18:46:10 +0900

    Update main_app.py

--- hg_app/main_app.py
+++ hg_app/main_app.py
@@ -1,19 +1,20 @@
-
 import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
+from bs4 import BeautifulSoup
 import os
 import sqlalchemy as sa
 import aggrid_option
-
+import csvimport
+import requests
 
 st.set_page_config(page_title="MainPage", layout="wide") 
 themetemplate = 'alpine'
 
 st.title("メインページ")
 
-col1, col2 = st.columns([20,10])
+col1, col2 = st.columns([12,20])
 with col1:
     st.subheader('【出荷件数チェック】')
     st.text(f'日々の出荷データ件数を集計出来ます。\n旧商品が含まれている場合、含まれている旧商品情報を表示させます。')
@@ -21,9 +22,36 @@ with col1:
     st.subheader('【商品別個数集計】')
     st.text(f'日々の出荷データ内の商品別個数を集計出来ます。')
 
-
-with col2:
+    st.subheader('【旧商品登録】')
     uploaded_file  = st.file_uploader('旧商品ファイルアップロード', type='csv')
     if uploaded_file:
+        
+        csvimport.pandas_csv_import_hedder(uploaded_file,'olditem')
+
         df = pd.read_csv(uploaded_file)
         df.to_csv('OldItem.csv')
+
+
+with col2:
+    st.subheader("ヤマト重要ニューススクレイピング")   
+    r = requests.get("https://www.yamato-hd.co.jp/important/")
+    soup = BeautifulSoup(r.content, "html.parser")
+    
+    #必要な情報のクラスを指定して抽出
+    found = soup.find('div', class_='Main__content')
+
+    style = '''
+                <style>
+                    time.news__date{
+                        font-size: 24px;
+                    }
+
+                    h3.news__title{
+                        font-size: 18px;
+                    }
+                </style>)'''
+
+    # print(f'{found}{style}')
+    
+    found = (str(found).replace('href="/', 'href="https://www.yamato-hd.co.jp/'))
+    st.write(f'{found}{style}', unsafe_allow_html=True)
